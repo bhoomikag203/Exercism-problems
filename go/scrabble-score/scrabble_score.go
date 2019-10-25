dcab9f76e53c72fabc12e166e2eff6569cd3544a
bhoomikag203 <bhoomikag203@gmail.com>
2019-10-25 15:22:21 +0530

    Refactor | removed ToUpper method, which increased the performance even more

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,56 +1,23 @@
 package scrabble
 
-import "unicode"
-
-//LetterToValueMap is a map of letter to values
-// var LetterToValueMap = map[rune]int{
-// 	'A': 1,
-// 	'E': 1,
-// 	'I': 1,
-// 	'O': 1,
-// 	'U': 1,
-// 	'L': 1,
-// 	'N': 1,
-// 	'R': 1,
-// 	'S': 1,
-// 	'T': 1,
-// 	'D': 2,
-// 	'G': 2,
-// 	'B': 3,
-// 	'C': 3,
-// 	'M': 3,
-// 	'P': 3,
-// 	'F': 4,
-// 	'H': 4,
-// 	'V': 4,
-// 	'W': 4,
-// 	'Y': 4,
-// 	'K': 5,
-// 	'J': 8,
-// 	'X': 8,
-// 	'Q': 10,
-// 	'Z': 10,
-// }
-
 //Score function compute the scrabble score for a given word
 func Score(input string) int {
 	score := 0
 	for _, v := range input {
-		v = unicode.ToUpper(v)
 		switch v {
-		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T', 'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't':
 			score++
-		case 'D', 'G':
+		case 'D', 'G', 'd', 'g':
 			score += 2
-		case 'B', 'C', 'M', 'P':
+		case 'B', 'C', 'M', 'P', 'b', 'c', 'm', 'p':
 			score += 3
-		case 'F', 'H', 'V', 'W', 'Y':
+		case 'F', 'H', 'V', 'W', 'Y', 'f', 'h', 'v', 'w', 'y':
 			score += 4
-		case 'K':
+		case 'K', 'k':
 			score += 5
-		case 'J', 'X':
+		case 'J', 'X', 'j', 'x':
 			score += 8
-		case 'Q', 'Z':
+		case 'Q', 'Z', 'q', 'z':
 			score += 10
 		}
 	}
