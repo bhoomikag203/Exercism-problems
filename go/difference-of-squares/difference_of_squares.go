90e56e98cfa4a175899937bdd30a4f957af9fa6f
bhoomikag203 <bhoomikag203@gmail.com>
2019-10-30 14:00:54 +0530

    Add | function to calculate difference of squares

---
+++ go/difference-of-squares/difference_of_squares.go
@@ -0,0 +1,21 @@
+package diffsquares
+
+func SquareOfSum(n int)int{
+    sum := 0
+    for i := 1 ; i <= n; i++ {
+    sum += i
+    }
+    return sum * sum
+}
+
+func SumOfSquares(n int)int{
+    sumOfSq := 0
+    for i := 1; i <= n; i++{
+        sumOfSq += i * i
+    }
+    return sumOfSq
+}
+
+func Difference(n int )int{
+    return SquareOfSum(n)-SumOfSquares(n)
+}
